31041368158bfa66479be7b2f3607bfb0baffcf6
高山泰宏 <adash-takayama@outlook.jp>
2016-12-23 08:13:03 +0900

    オリジナルゲームSlopeJumpのデータです。
    BGMとジャンプ時の効果音を実装しました。
    またスマートフォンでのタップ及び傾きでの操作を実装しましたが、今のままではジャンプ中に方向転換が出来ない為に未完成となっています。

--- Assets/C#/PlayerSCR.cs
+++ Assets/C#/PlayerSCR.cs
@@ -7,13 +7,14 @@ public class PlayerSCR : MonoBehaviour {
 
     Rigidbody2D rigid2D;
 
-    float jumpForce = 400.0f;
-    float walkForce = 30.0f;
-    float maxWalkSpeed = 2.0f;
-    float threshold = 0.2f;
+    float jumpForce = 400.0f;           // ジャンプの飛距離
+    float walkForce = 30.0f;            // 
+    float maxWalkSpeed = 2.0f;          // 徒歩での歩く速度
+    float threshold = 0.2f;             // 傾き度合いの判定
+
+    public AudioClip JumpSE;
 
     public int pointnow;
-
     public int point;
 
     // Use this for initialization
@@ -29,13 +30,14 @@ public class PlayerSCR : MonoBehaviour {
     void Update () {
 
         // スペースキーを押した時にジャンプする
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            GetComponent<AudioSource>().PlayOneShot(JumpSE);
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
         // 左右移動
         int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key =  -1;
+        if (Input.GetKey(KeyCode.RightArrow) || Input.acceleration.x > this.threshold) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.acceleration.x < this.threshold) key =  -1;
 
         // プレイヤーの速度
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
